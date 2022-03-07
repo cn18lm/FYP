a119a65c02f57aaa11d3019ef6ba89f324e7c8db
cn18lm <cn18lm@leeds.ac.uk>
2022-03-07 15:40:48 +0000

    More functions in test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #replace root[n] with search for specific child?
-
+import calendar
+daysOfWeek = list(calendar.day_name)
 
 from datetime import datetime
 
@@ -10,7 +11,7 @@ root = tree.getroot()
 SchedulingPeriod = root.attrib["ID"]
 filename = SchedulingPeriod + ".dzn"
 
-f = open(filename, "w",)
+f = open(filename, "w")
 
 # Useful functions
 
@@ -34,7 +35,23 @@ def writeSet(n, a):
             f.write(", " + str(item))
     
     f.write("}\n")
-        
+
+def dayStrToInt(d):
+    return daysOfWeek.index(d)
+
+def writeCalendar(n,a):
+    f.write(n + " = [|")
+    for i in a:
+        first = True
+        for j in i:
+            if first:
+                f.write(j)
+                first = False
+            else:
+                f.write(", " + j)
+        f.write(" |")
+    f.write("] \n")
+            
 
 for child in root:
   print(child.tag,child.attrib)
@@ -43,15 +60,16 @@ for child in root:
 writeComment(SchedulingPeriod)
 
 #StartDate
-StartDate = toDate(root[0].text)
+StartDate = toDate(root.find('StartDate').text)
 writeComment("StartDate: " + StartDate.strftime('%Y-%m-%d'))
 
-#Starting day,  monday = 0 
-print(StartDate.weekday())
-
 #EndDate
-EndDate = toDate(root[1].text)
+EndDate = toDate(root.find('EndDate').text)
 writeComment("EndDate: " + EndDate.strftime('%Y-%m-%d'))
+
+#Starting day,  monday = 0 
+firstDay = StartDate.weekday()
+writeVar("firstDay",firstDay)
 
 #numDays
 delta = EndDate - StartDate
@@ -60,25 +78,38 @@ writeVar("numDays", numDays)
 
 #Skills
 Skills = []
-for child in root[2]:
+for child in root.find('Skills'):
   Skills.append(child.text)
 writeSet("Skills", Skills)
 
 #ShiftTypes
 ShiftTypes = []
 
-for child in root[3]:
+for child in root.find('ShiftTypes'):
   name = child.attrib["ID"]
   ShiftTypes.append(name)
-  skills = []
-  for skill in child[3]:
-      skills.append(skill.text)
-  writeSet(name, skills)
+  arr = []
+  for skill in child.find('Skills'):
+      arr.append(skill.text)
+  writeSet(name, arr)
 writeSet("ShiftTypes", ShiftTypes)
 
 #CoverRequirements
-CoverRequirements = []
+week = daysOfWeek.copy()
+for x in root.find('CoverRequirements'):
+    if x.tag == 'DayOfWeekCover':
+        day = ShiftTypes.copy()
+        for y in x.findall('Cover'):
+            index = ShiftTypes.index(y[0].text)
+            day[index] = y[1].text
+        index = daysOfWeek.index(x[0].text)
+        week[index] = day
 
+big = []
 
+for i in range(numDays):
+    big.append(week[(firstDay + i)%7])
+
+writeCalendar("CoverRequirements", big)
 
 f.close()
